0d90bea0888d84325145d705b1e312395457f327
Lucas Estevam <86022472+luuckz@users.noreply.github.com>
2021-11-16 19:18:25 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider_principal').slick({
+    dots: true, 
+    infinite: true,
+    speed: 300,
+    sliderToShow: 2,
+    adaptiveHeight: true,
+    autoplay: true,
+    sutoplaySpeed: 2000
+});
